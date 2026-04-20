e0bd621f31686737d4ef5a732f6b5452267d9024
Unitedglobalexpress <info@unitedglobalexpress.com>
2026-04-20 14:05:25 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,15 @@
 export const metadata = {
   title: "United Global Express LLC",
   description:
-    "Warehousing, pallet storage, fulfillment, cross-docking, container unloading and logistics services in New Jersey.",
+    "Warehousing, pallet storage, fulfillment, cross-docking, container unloading",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="<meta name="google-site-verification" content="6siwQgMp4yEf6xkxrSdtQwwxydnA6lkB479bW0p5oLE" />" />
+      </head>
       <body style={{ margin: 0, fontFamily: "Arial" }}>
         {children}
       </body>
