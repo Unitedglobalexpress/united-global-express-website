3f78dbcd7f80893c8940bbf88b2bebdf07f08650
Unitedglobalexpress <info@unitedglobalexpress.com>
2026-04-20 14:07:20 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta name="google-site-verification" content="<meta name="google-site-verification" content="6siwQgMp4yEf6xkxrSdtQwwxydnA6lkB479bW0p5oLE" />" />
+        <meta
+          name="google-site-verification"
+          content="6siwQgMp4yEf6xkxrSdtQwwxdynA6LkB479bW0p5oLE"
+        />
       </head>
       <body style={{ margin: 0, fontFamily: "Arial" }}>
         {children}
